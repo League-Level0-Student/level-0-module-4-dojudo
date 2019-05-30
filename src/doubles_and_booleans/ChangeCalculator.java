38fff6230ae0abdefb291bf9f75e3c86d72068b0
ilabstaff <ilabstaff@172.16.116.81>
2019-05-29 18:01:45 -0700

    we ended on sleepy head

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,26 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
+String nickles =JOptionPane.showInputDialog(null,"how many nickels do you have");
+	
 
 		// Convert their answer to an int using Integer.parseInt()
-
+     int n =  Integer.parseInt(nickles);
 		// Ask the user how many dimes they have, and convert their answer
-
-		// Ask the user how many quarters they have, and convert their answer
-
+String dimes =JOptionPane.showInputDialog("how many dimes do you have");
+		int d = Integer.parseInt(dimes);
+			
+		
+// Ask the user how many quarters they have, and convert their answer
+String quarters =JOptionPane.showInputDialog("how many quarters do you have");
 		// Calculate how much money the user has and save it in a double variable 
-
+int q = Integer.parseInt(quarters);
 		// Tell the user how much money they have
-
+int total = n*5+d*10+q*25; 
+System.out.println(total);
+double dollars = (double)total/100;
+System.out.println(dollars);
+	JOptionPane.showMessageDialog(null,"you have "+dollars);
 	}
 }
 
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,3 +1,4 @@
+
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
@@ -18,13 +19,21 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
+        int i =JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+        System.out.println(i);
+        if(i==0) {
+        int j =JOptionPane.showConfirmDialog(null, "Is it a vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
 
+	JOptionPane.showMessageDialog(null,"get up lazy bones");
 
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
+}
+else {
+	JOptionPane.showMessageDialog(null,"sleep in");
+}
+/*
+         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
+         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
+         * print “sleep in�?.
          */
     }
 }
